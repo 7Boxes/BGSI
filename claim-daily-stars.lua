62d1f926611a23fdfcd5f8e9228b2cf07e05ae2c
7Boxes. <arana.gastador.9r@icloud.com>
2025-05-15 20:12:04 -0500

    Create claim-daily-stars.lua

---
+++ claim-daily-stars.lua
@@ -0,0 +1,13 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local remotePath = ReplicatedStorage:WaitForChild("Shared")
+    :WaitForChild("Framework")
+    :WaitForChild("Network")
+    :WaitForChild("Remote")
+    :WaitForChild("RemoteEvent")
+
+local args = {"DailyRewardClaimStars"}
+
+while true do
+    remotePath:FireServer(unpack(args))
+    wait(1) -- Wait for 1 second before firing again
+end
